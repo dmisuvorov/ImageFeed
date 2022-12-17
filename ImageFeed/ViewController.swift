0e508dd24d29171554f591330ca82269c758f0cc
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-12-17 14:40:24 +0300

    Initial commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
