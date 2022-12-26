623f5b57933e4deade900e7fae62ab9a823d3aef
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-12-26 23:53:41 +0300

    Верстка экрана с таблицей картинок

--- ImageFeed/ImageListViewController.swift
+++ ImageFeed/ImageListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImageListViewController.swift
 //  ImageFeed
 //
 //  Created by Суворов Дмитрий Владимирович on 16.12.2022.
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImageListViewController: UIViewController {
 
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
