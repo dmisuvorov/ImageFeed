2dfc6a498ab500fa6f7f554a88661a84d860ba7e
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-12-27 08:33:54 +0300

    Подготовка делегата для таблицы с картинками

--- ImageFeed/ImageListViewController.swift
+++ ImageFeed/ImageListViewController.swift
@@ -13,9 +13,24 @@ class ImageListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ImageListViewController: UITableViewDelegate {
+    
+}
+
+extension ImageListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
