0e0e2f487e8274269cd9e74684769191a3ac883b
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-16 14:04:06 +0300

    Переход с таблицы к SingleImageView

---
+++ ImageFeed/Presentation/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 16.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+}
