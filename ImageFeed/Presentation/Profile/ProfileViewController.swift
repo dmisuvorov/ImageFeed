9a7b1db5e8695f2992160532dbe59188f5cf9fa6
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-22 23:41:57 +0300

    Создал view-controller для профиля

---
+++ ImageFeed/Presentation/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 22.01.2023.
+//
+
+import UIKit
+
+class ProfileViewController : UIViewController {
+    
+}
