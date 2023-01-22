b794ef136d31d0b0788ec1f122918d53bd719ee1
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-23 00:27:31 +0300

    Работа с цветами в коде

---
+++ ImageFeed/Helpers/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 23.01.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black")! }
+    static var ypBlackAlpha0: UIColor { UIColor(named: "YP Black (Alpha 0)")! }
+    static var ypBackground: UIColor { UIColor(named: "YP Background")! }
+    static var ypGray: UIColor { UIColor(named: "YP Gray")! }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue")! }
+    static var ypRed: UIColor { UIColor(named: "YP Red")! }
+    static var ypWhite: UIColor { UIColor(named: "YP White")! }
+    static var ypWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)")! }
+}
