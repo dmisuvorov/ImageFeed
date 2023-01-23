65d834af3d80dbc027eae9798104d9db8292ecdb
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-23 11:36:36 +0300

    Конфигурация view-property на ProfileViewController

--- ImageFeed/Presentation/Profile/ProfileViewController.swift
+++ ImageFeed/Presentation/Profile/ProfileViewController.swift
@@ -22,6 +22,33 @@ class ProfileViewController : UIViewController {
     private lazy var nameLabel: UILabel = {
         let nameLabel = UILabel()
         nameLabel.text = "Екатерина Новикова"
-        
-    }
+        nameLabel.font = UIFont.ypBold23
+        nameLabel.textColor = UIColor.ypWhite
+        nameLabel.numberOfLines = 1
+        nameLabel.adjustsFontSizeToFitWidth = true
+        return nameLabel
+    }()
+    
+    private lazy var nicknameLabel: UILabel = {
+        let nicknameLabel = UILabel()
+        nicknameLabel.text = "@ekaterina_nov"
+        nicknameLabel.font = UIFont.ypRegular13
+        nicknameLabel.textColor = UIColor.ypGray
+        return nicknameLabel
+    }()
+    
+    private lazy var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+        statusLabel.text = "Hello, world!"
+        statusLabel.font = UIFont.ypRegular13
+        statusLabel.textColor = UIColor.ypWhite
+        return statusLabel
+    }()
+    
+    private lazy var logoutButton: UIButton = {
+        let logoutButton = UIButton()
+        logoutButton.setImage(UIImage(named: "logout_button"), for: UIControl.State.normal)
+        logoutButton.tintColor = UIColor.ypRed
+        return logoutButton
+    }()
 }
