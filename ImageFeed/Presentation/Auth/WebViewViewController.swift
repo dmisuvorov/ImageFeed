c6782f7a502219c680f07119e6a1bb83679a9f5e
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-30 13:31:47 +0300

    Создан новый класс WebViewViewController для показа WebView

---
+++ ImageFeed/Presentation/Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 30.01.2023.
+//
+
+import UIKit
+
+class WebViewViewController : UIViewController {
+    
+}
