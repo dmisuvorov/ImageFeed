237d75e1eaf922127fe71cb587993ede6fdf0db5
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-01-30 13:27:06 +0300

    Создан класс AuthViewController для связи соответствующего ViewController из storyboard

---
+++ ImageFeed/Presentation/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 30.01.2023.
+//
+
+import UIKit
+
+class AuthViewController : UIViewController {
+    
+}
