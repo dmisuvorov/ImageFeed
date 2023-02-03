34f9d2a32b00865a14cfedcda3e761cb2141021a
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-02-03 13:53:06 +0300

    В Main.storyboard добавлен SplashViewController + верстка + связка с соответствующим классом

---
+++ ImageFeed/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SplashViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 03.02.2023.
+//
+
+import UIKit
+
+class SplashViewController : UIViewController {
+    
+}
