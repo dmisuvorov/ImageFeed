8ad94d2d51acd5af789a33e55fceb967acf082d3
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-02-05 23:08:50 +0300

    Добавил константы для авторизации

--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-let AccessKey = "<ваш Access Key>"
-let SecretKey = "<ваш Secret Key>"
-let RedirectURI = "<ваш Redirect URI>"
+let AccessKey = "1BIWWhAo5Btj9-tsSV7qNXkgqxVR2ilNKRtZBRgSkBQ"
+let SecretKey = "WKdg9rSB9sUUEbyp9ih3BKfaHfy7INcu5WvOGdGW3Tg"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let AccessScope = "public+read_user+write_likes"
 let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+let UnsplashTokenURL = URL(string: "https://unsplash.com/oauth/token")!
 let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 
 let Empty = ""
