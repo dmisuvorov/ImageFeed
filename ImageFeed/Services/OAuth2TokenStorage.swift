5fff8f27fcac2b3bff9d734ed3d8ff57d87eeaf2
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-02-16 21:51:50 +0300

    Замена использования OAuth2TokenStorage на хранение и получение из хранилища токена с использованием SwiftKeychainWrapper

--- ImageFeed/Services/OAuth2TokenStorage.swift
+++ ImageFeed/Services/OAuth2TokenStorage.swift
@@ -5,22 +5,22 @@
 //  Created by Суворов Дмитрий Владимирович on 03.02.2023.
 //
 
-import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
     static let shared = OAuth2TokenStorage()
     
-    private let userDefaults = UserDefaults.standard
+    private let keychainWrapper = KeychainWrapper.standard
     
     var token: String {
         get {
-            guard let data = userDefaults.string(forKey: Keys.token.rawValue) else {
+            guard let data = keychainWrapper.string(forKey: Keys.token.rawValue) else {
                 return .init()
             }
             return data
         }
         set {
-            userDefaults.set(newValue, forKey: Keys.token.rawValue)
+            keychainWrapper.set(newValue, forKey: Keys.token.rawValue)
         }
     }
     
