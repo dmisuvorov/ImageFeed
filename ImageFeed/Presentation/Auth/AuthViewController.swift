9fb0e6f026a04d711ac1c6341f5ffb7da91847ef
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-02-18 22:51:13 +0300

    AuthViewController - final класс

--- ImageFeed/Presentation/Auth/AuthViewController.swift
+++ ImageFeed/Presentation/Auth/AuthViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AuthViewController : UIViewController {
+final class AuthViewController : UIViewController {
     var delegate: AuthViewControllerDelegate?
     
     private let ShowWebViewSegueId = "ShowWebView"
