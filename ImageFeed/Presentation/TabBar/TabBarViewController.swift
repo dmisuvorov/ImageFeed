ade68041b49af04c31b76d40c6a2168d90659e69
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2023-02-18 13:56:09 +0300

    Инициализация ProfileViewController из кода

---
+++ ImageFeed/Presentation/TabBar/TabBarViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TabBarViewController.swift
+//  ImageFeed
+//
+//  Created by Суворов Дмитрий Владимирович on 18.02.2023.
+//
+
+import UIKit
+
+final class TabBarViewController : UITabBarController {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        let imagesListViewController = storyboard
+            .instantiateViewController(withIdentifier: "ImagesListViewController")
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: NSLocalizedString("Profile", comment: Empty),
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+        
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+}
